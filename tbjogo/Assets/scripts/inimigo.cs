d714a7c72f5be4327bf73c336e91e748e21a2e85
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2023-10-26 15:55:09 -0300

    tentando fazer o ataque

--- tbjogo/Assets/scripts/inimigo.cs
+++ tbjogo/Assets/scripts/inimigo.cs
@@ -1,93 +1,47 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class inimigo : MonoBehaviour
 {
-    public float speed;
+
+    public float Speed;
     public float walkTime;
-
     private float timer;
-    private bool walkRihgt = true;
+    public bool walkRight;
 
-    public int vida;
-
-    private Animator anima;
     private Rigidbody2D rig;
-
-    public float ataque = 2.0f;
-    public int damage = 10;
-
-    private Transform player;
-    private Animator anim;
-    private bool isAttacking = false;
-
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        anima = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         timer += Time.deltaTime;
 
         if (timer >= walkTime)
         {
-            walkRihgt = !walkRihgt;
+            walkRight = !walkRight;
             timer = 0f;
         }
 
-        if (walkRihgt)
+        if (walkRight)
         {
-            transform.eulerAngles = new Vector2(0, 0);
-            rig.velocity = Vector2.right * speed;
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * Speed; 
         }
         else
         {
-            transform.eulerAngles = new Vector2(0, 180);
-            rig.velocity = Vector2.left * speed;
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.right * Speed;
         }
     }
 
-    public void Damage(int dmg)
-    {
-        vida -= dmg;
-        anim.SetTrigger("hit");
-
-        if (vida <= 0)
-        {
-            Destroy(gameObject);
-        }
+    void ataque() {
+        transform.tag = "Player";
     }
-
-    private void Update()
-    {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-
-        if (distanceToPlayer <= ataque && !isAttacking)
-        {
-            // Iniciar a animação de ataque
-            anima.SetTrigger("Ataque"); // Acione o trigger de ataque
-            isAttacking = true;
-        }
-    }
-
-    public void PerformAttack()
-    {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-
-        if (distanceToPlayer <= ataque)
-        {
-
-        }
-    }
+    
 }
-
-
