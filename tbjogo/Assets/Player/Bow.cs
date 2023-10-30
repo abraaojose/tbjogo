ff0aa383094f6baa0518f736e2102726fcb98b11
Vinicius Abraão Bezerra do Nascimento <20211170150039@ifrn.local>
2023-10-30 10:29:37 -0300

    arrumando

--- tbjogo/Assets/Player/Bow.cs
+++ tbjogo/Assets/Player/Bow.cs
@@ -1,23 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
-
     private Rigidbody2D rig;
-    public float speed; 
-    
+    public float speed;
+    private bool canMove = false;
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        Invoke("EnableMovement", 0.6f); // Chama o método EnableMovement após 0.6 segundos
         Destroy(gameObject, 2f);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        rig.velocity = Vector2.right * speed;
+        if (canMove)
+        {
+            rig.velocity = Vector2.right * speed;
+        }
+    }
+
+    void EnableMovement()
+    {
+        canMove = true;
     }
 }
