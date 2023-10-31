f0e8dfe03960c73e5e7d89be1edc50f4bc541503
Vinicius Abraão Bezerra do Nascimento <20211170150039@ifrn.local>
2023-10-31 08:17:23 -0300

    arrumando

--- tbjogo/Assets/Player/Scripits/Player.cs
+++ tbjogo/Assets/Player/Scripits/Player.cs
@@ -9,25 +9,28 @@ public class Player : MonoBehaviour
     public float jumpForce;
     public bool pulan;
     private bool isFire;
+    private bool canFire = true; // Nova variável para controle do disparo
     public GameObject Bowprefab;
     public Transform firePonit;
 
     private Rigidbody2D rig;
     private Animator anim;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Jump();
-        BowFire();
+        
+        // Somente chama a função de disparo se estiver no estado Idle
+        if (Input.GetKeyDown(KeyCode.F) && !isFire && anim.GetInteger("Transition") == 0)
+        {
+            StartCoroutine("Fire");
+        }
     }
 
     private void FixedUpdate()
@@ -46,7 +49,7 @@ public class Player : MonoBehaviour
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
-        if (movimento < 0 && !pulan )
+        if (movimento < 0 && !pulan)
         {
             anim.SetInteger("Transition", 1);
             transform.eulerAngles = new Vector3(0, 180, 0);
@@ -67,38 +70,45 @@ public class Player : MonoBehaviour
                 anim.SetInteger("Transition", 2);
                 rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
                 pulan = true;
-                
             }
         }
-            
     }
-
 
     void BowFire()
     {
         StartCoroutine("Fire");
     }
+
     void fogo()
     {
         GameObject bow = Instantiate(Bowprefab, firePonit.position, firePonit.rotation);
         Rigidbody2D rb = bow.GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.right * 11f;
+
+        // Verificação da direção do personagem
+        if (transform.eulerAngles.y == 0) // Personagem está voltado para a direita
+        {
+            rb.velocity = Vector2.right * 11f; // Bola de fogo vai para a direita
+        }
+        else // Personagem está voltado para a esquerda
+        {
+            rb.velocity = Vector2.left * 11f; // Bola de fogo vai para a esquerda
+        }
+
         Destroy(bow, 2f);
     }
-
+    
     IEnumerator Fire()
     {
-        if (Input.GetKeyDown((KeyCode.F)))
-        {
-            isFire = true;
-            anim.SetInteger("Transition", 3);
-            Invoke("fogo", 0.5f);
-            yield return new WaitForSeconds(0.8f);
-            anim.SetInteger("Transition",0);
-            isFire = false;
-        }
+        isFire = true;
+        anim.SetInteger("Transition", 3);
+        Invoke("fogo" ,0.5f);// Chama o método de disparo imediatamente
+        canFire = false; // Bloqueia temporariamente o disparo
+        yield return new WaitForSeconds(0.8f);
+        anim.SetInteger("Transition", 0);
+        isFire = false;
+        canFire = true; // Libera o disparo novamente
     }
-    
+
     void OnTriggerStay2D(Collider2D coll)
     {
         if (coll.gameObject.layer == 8)
@@ -106,5 +116,4 @@ public class Player : MonoBehaviour
             pulan = false;
         }
     }
-    
 }
