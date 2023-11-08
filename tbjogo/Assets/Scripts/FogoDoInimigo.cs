5d04f9641d94a802685d9aecf7da5e5c8e72e41f
Júlia Emmylly de Lima Monteiro <20211170150007@ifrn.local>
2023-11-08 11:53:31 -0300

    musica pronta e barra de vida incompleta

--- tbjogo/Assets/Scripts/FogoDoInimigo.cs
+++ tbjogo/Assets/Scripts/FogoDoInimigo.cs
@@ -8,11 +8,19 @@ public class FogoDoInimigo : MonoBehaviour
     public float velocidadeDoFogo;
     
     public float tempoDoFogo;
+    
+    private AudioSource sound;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, tempoDoFogo);
+        sound.Play();
+        Destroy(this.gameObject, 2f );
+    }
+
+    private void Awake()
+    {
+        sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
